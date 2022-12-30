038f4abfc7a6b6adcd1b23bf7660b0ff7b62f512
Pavel Pscheidl <pavel2@pscheidl.cz>
2022-12-30 18:45:06 +0100

    feat: derive(Enumerated) macro as an alternative to existing attribute macro

--- enum-collections-macros/src/lib.rs
+++ enum-collections-macros/src/lib.rs
@@ -42,3 +42,39 @@ pub fn enum_collections(_args: TokenStream, input: TokenStream) -> TokenStream {
     };
     TokenStream::from(output.to_token_stream())
 }
+
+/// Creates `enum_map::Enumerated` implementation for the underlying Enum.
+/// Also derives Copy and Clone.
+#[proc_macro_derive(Enumerated)]
+pub fn derive_enum_collections(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let generics = &input.generics;
+    let name = &input.ident;
+    let enum_count: usize = match &input.data {
+        syn::Data::Enum(en) => en.variants.iter().count(),
+        syn::Data::Struct(_) | syn::Data::Union(_) => {
+            return quote! {
+                #input
+                compile_error!("The `enummap` macro only supports enums.");
+            }
+            .to_token_stream()
+            .to_token_stream()
+            .into();
+        }
+    };
+
+    let output = quote! {
+        impl #generics Enumerated for #name #generics {
+
+            fn position(&self) -> usize {
+                *self as usize
+            }
+
+            fn len() -> usize{
+                #enum_count
+            }
+
+        }
+    };
+    TokenStream::from(output.to_token_stream())
+}
--- enum-collections/src/lib.rs
+++ enum-collections/src/lib.rs
@@ -6,6 +6,7 @@ pub use crate::enumerated::Enumerated;
 pub use crate::enummap::EnumMap;
 pub use crate::enumtable::EnumTable;
 pub use enum_collections_macros::enum_collections;
+pub use enum_collections_macros::Enumerated;
 
 #[cfg(test)]
 mod tests {
@@ -13,8 +14,21 @@ mod tests {
     use enum_collections_macros::enum_collections;
 
     #[test]
-    fn test() {
+    fn test_attribute_macro() {
         #[enum_collections]
+        enum Letter {
+            A,
+            B,
+        }
+
+        assert_eq!(0, Letter::A.position());
+        assert_eq!(1, Letter::B.position());
+        assert_eq!(2, Letter::len());
+    }
+
+    #[test]
+    fn test_derive_macro() {
+        #[derive(Enumerated, Clone, Copy)]
         enum Letter {
             A,
             B,
