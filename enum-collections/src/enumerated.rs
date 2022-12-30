77481587b4097931e70407a0945d296a8c307880
Pavel Pscheidl <pavel2@pscheidl.cz>
2022-12-30 09:53:05 +0100

    docs: Basic documentation for the `Enumerated` trait

--- enum-collections/src/enumerated.rs
+++ enum-collections/src/enumerated.rs
@@ -1,4 +1,18 @@
+/// Provides means to map enum  values to positions in arrays backing an EnumMap/EnumTable.
+/// Not intended to be implemented by hand. Annotating an enum with the `#[enum_collections]`
+/// attribute macro is preferred.
+///
+/// ```
+///  use enum_collections::{enum_collections, Enumerated};
+/// #[enum_collections]
+/// enum Letter {
+///     A,
+///     B,
+/// }
+/// ```
 pub trait Enumerated {
+    /// Maps an enum to a unique position in an array.
     fn position(&self) -> usize;
+    /// Total number of values in an Enum.
     fn len() -> usize;
 }
