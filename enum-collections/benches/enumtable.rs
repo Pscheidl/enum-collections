e99b0319c8355ecca6c1d914bfa6a97143bc2ea0
Pavel Pscheidl <pavel2@pscheidl.cz>
2023-01-15 20:58:39 +0100

    chore: Remove enumtable GET and INSERT benchmarks

--- enum-collections/benches/enumtable.rs
+++ enum-collections/benches/enumtable.rs
@@ -16,29 +16,6 @@ enum Letter {
     G,
 }
 
-fn enumtable_get(criterion: &mut Criterion) {
-    let mut enum_map: EnumTable<Letter, u8> = EnumTable::new();
-    enum_map.insert(Letter::A, 1);
-    criterion.bench_function("EnumTable get", |bencher| {
-        bencher.iter(|| enum_map.get(black_box(Letter::A)))
-    });
-}
-
-fn enumtable_insert(criterion: &mut Criterion) {
-    let mut enum_map: EnumTable<Letter, u8> = EnumTable::new();
-    criterion.bench_function("EnumTable insert", |bencher| {
-        bencher.iter(|| enum_map.insert(black_box(Letter::A), black_box(1)))
-    });
-}
-
-fn enumtable_index_get(criterion: &mut Criterion) {
-    let mut enum_map: EnumTable<Letter, u8> = EnumTable::new();
-    enum_map.insert(Letter::A, 1);
-    criterion.bench_function("EnumTable Index get", |bencher| {
-        bencher.iter(|| enum_map[black_box(Letter::A)])
-    });
-}
-
 fn enumtable_index_mut_insert(criterion: &mut Criterion) {
     let mut enum_map: EnumTable<Letter, u8> = EnumTable::new();
     criterion.bench_function("EnumTable IndexMut insert", |bencher| {
@@ -73,9 +50,6 @@ fn enumtable_init(criterion: &mut Criterion) {
 
 criterion_group!(
     benches,
-    enumtable_get,
-    enumtable_insert,
-    enumtable_index_get,
     enumtable_index_mut_insert,
     enum_map_crate_get,
     enum_map_crate_insert,
